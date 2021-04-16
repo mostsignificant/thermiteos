0249486c9043df8b29eb8720ed1c7e207fe2b927
Christian Göhring <cg@2601.me>
2021-04-16 16:30:27 +0200

    packaging in bootimage

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static WELCOME: &[u8] = b"Welcome to thermiteos!";
+
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in WELCOME.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
