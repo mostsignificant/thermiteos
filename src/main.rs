7d46d84df873625cc2fc9c7625c0b7bc9db6eba7
Christian Göhring <cg@2601.me>
2021-04-16 17:27:04 +0200

    extended vga buffer support

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
 use core::panic::PanicInfo;
-
-static WELCOME: &[u8] = b"Welcome to thermiteos!";
-
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in WELCOME.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga::print_welcome();
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
